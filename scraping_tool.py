ffdaf16c3228cdcd372a1d6aae4d582e538c87ad
hiraokakoichi <the10pointsflavor0327@gmail.com>
2024-02-16 12:43:57 +0900

    さしあたり動作するように修正

--- scraping_tool.py
+++ scraping_tool.py
@@ -1,4 +1,5 @@
 import datetime
+import time
 
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
@@ -75,9 +76,16 @@ def main():
 
                     # registered_at
                     data[0] = TODAY
+                    logger.debug("registered_atまで取得")
                     # job_id
-                    data[1] = find_element(elem, By.CLASS_NAME, "jcs-JobTitle").get_attribute("data-jk")
+                    try:
+                        data[1] = find_element(elem, By.CLASS_NAME, "jcs-JobTitle").get_attribute("data-jk")
+                    except Exception:
+                        print("data[1]が見つからない")
+                        time.sleep(600)
+                        pass
 
+                    logger.debug("job_idまで取得")
                     # 登録済みならスキップ
                     if data[1] in registered_job_ids:
                         logger.info("Skip registered job id")
@@ -86,17 +94,21 @@ def main():
                         continue
                     else:
                         registered_job_ids.append(data[1])
+                    
 
                     # job_title
                     data[2] = find_element(elem, By.CLASS_NAME, "jobTitle").text
+                    logger.debug("job_titleまで取得")
                     # company_name
-                    data[3] = find_element(elem, By.CSS_SELECTOR, "a[data-tn-element='companyName']").text
+                    data[3] = find_element(elem, By.CSS_SELECTOR, "a[data-tn-element='companyName']").text#find_element(elem, By.CLASS_NAME, "companyName").text
+                    logger.debug("company_nameまで取得")
                     # company_location
-                    data[4] = find_element(elem, By.CSS_SELECTOR, "div[data-testid='icon-location'] > div").text
+                    data[4] = "取得不能"#find_element(elem, By.CSS_SELECTOR, "div[data-testid='icon-location'] > div").text#",".join([find_element(elem, By.CLASS_NAME, "companyLocation").text])
+                    logger.debug("company_locationまで取得")
 
                     url = None
                     try:
-                        url = find_element(elem, By.CLASS_NAME, "more_loc").get_attribute("href")
+                        url = None #find_element(elem, By.CLASS_NAME, "more_loc").get_attribute("href")
                     except NoSuchElementException:
                         pass
 
@@ -125,10 +137,12 @@ def main():
                     # application_tag
                     data[7] = ",".join([x.text for x in find_elements(elem, By.CSS_SELECTOR, ".jobsearch-JobCard-tagContainer > span")])
                     print("application_tag取得")
+                    # ssdb.insert_data(data)
 
                     # job_snippet
-                    data[8] = "取得不可"
+                    data[8] = "取得不可"#find_element(elem, By.CLASS_NAME, "job-snippet").text
                     print("job_snippet取得")
+                    # ssdb.insert_data(data)
 
                     try:
                         url = f"https://jp.indeed.com/viewjob?jk={data[1]}"
@@ -138,18 +152,21 @@ def main():
                         # job_tag
                         data[9] = ",".join([x.text.replace(",", "_") for x in find_elements(d, By.CLASS_NAME, "css-8fx8lh")])
                         print("job_tag取得")
+                        # ssdb.insert_data(data)
+                        logger.debug("job_tagまで取得")
                         ssdb.insert_data(data)
-                        print("インサート！")
-
                     finally:
+                        logger.debug("finallyを実行")
                         d.close()
                         d.switch_to.window(d.window_handles[0])  # switch original tab
 
                     logger.info(f"Register job id={data[1]} | {data[2]}")
+                    # print("インサート！")
+                    # ssdb.insert_data(data)
 
                 except NoSuchElementException:
                     # ポップアップ閉じるための処理
-                    #find_element(d, By.CLASS_NAME, "css-yi9ndv").click()
+                    # find_element(d, By.CLASS_NAME, "css-yi9ndv").click()
                     logger.info("Close popup")
                     continue
                 except Exception as e:
